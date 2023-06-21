ae5b50a33d3c2468190f60e4bb113642e8195df1
Omprakash Rahangdale <35596374+OmprakashR@users.noreply.github.com>
2023-06-21 09:37:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 function ValidateEmail(input) {
-  var validRegex =
-  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  var validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+
 
   if (input.value.match(validRegex)) {
     document.form1.text1.focus();
     document.getElementById("emailval").classList.remove("email-val");
     showmodel();
   } else {
-    document.form1.text1.focus();
+     
     document.getElementById("emailval").classList.add("email-val");
   }
 }
 
+ 
 function showmodel() {
+
+  var x = document.getElementById("inputsub").value;
+  document.getElementById("emailtext").innerHTML = x;
+
+
   document.getElementById("thankyou").style.display = "flex";
+
+
+ 
 }
 
 function hidebox() {
   document.getElementById("thankyou").style.display = "none";
 }
+
+
+ 
